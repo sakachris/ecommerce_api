5e4502be388af0349aad7051aba6226c649e2336
sakachris <sakachris90@gmail.com>
2025-11-30 21:19:09 +0300

    removing session authentication globally

--- ecommerce_api/settings.py
+++ ecommerce_api/settings.py
@@ -72,7 +72,7 @@ AUTH_USER_MODEL = "catalogue.User"
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework.authentication.SessionAuthentication",
+        # "rest_framework.authentication.SessionAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "DEFAULT_PAGINATION_CLASS": (
